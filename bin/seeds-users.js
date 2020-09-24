0477fbfdebcfa70077a8548954bf04888071e6e4
Bouziane <bey.bouziane@live.fr>
2020-09-24 16:15:29 +0200

    Seeds for tags and users

---
+++ bin/seeds-users.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const User = require("../models/User");
+const mongoose = require("mongoose");
+
+const Users = [
+  {
+    name: "Remy",
+    lastname: "LaFourmi",
+    email: "RemyLF@gmail.mail",
+    password: "Fourmi",
+  },
+  {
+    name: "Celine",
+    lastname: "Gonzales",
+    email: "CelineG@gmail.mail",
+    password: "abc123",
+  },
+  {
+    name: "Erwann",
+    lastname: "LePaon",
+    email: "ErwannL@gmail.mail",
+    password: "def123",
+  },
+];
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((self) => {
+    User.create(users)
+      .then((dbResult) => {
+        console.log(dbResult);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
