eccb6527de3091fae225a81c42d1ae61f0448536
Bouziane <bey.bouziane@live.fr>
2020-09-24 16:02:01 +0200

    Tag User model done

---
+++ models/Tag.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tagsSchema = new Schema({
+  label: String,
+});
+
+const Tags = mongoose.model("Tags", tagsSchema);
+module.exports = Tags;
