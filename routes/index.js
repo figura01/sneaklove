392e13419eb59732ae032f6d4808fae927be34a6
Bouziane <bey.bouziane@live.fr>
2020-09-24 15:53:03 +0200

    Link ok

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/sneakers/:cat", (req, res) => {
-  res.send("bar");
+router.get("/home", (req, res) => {
+  res.render("index");
 });
 
-router.get("/one-product/:id", (req, res) => {
-  res.send("baz");
+router.get("/sneakers/:cat", (req, res) => {
+  res.render("products_manage.hbs");
 });
 
 router.get("/signup", (req, res) => {
@@ -19,6 +19,10 @@ router.get("/signup", (req, res) => {
 
 router.get("/signin", (req, res) => {
   res.send("love");
+});
+
+router.get("/one-product/:id", (req, res) => {
+  res.send("baz");
 });
 
 module.exports = router;
