226c2a4b3982da2f0a0fc335d00bf0970e5754e1
Bouziane <bey.bouziane@live.fr>
2020-09-24 15:04:13 +0200

    Bug fix on config part

--- config/mongo.js
+++ config/mongo.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.FAKE_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.on("connected", () => console.log("yay mongodb connected :)"));
+mongoose.connection.on("connected", () =>
+  console.log("yay mongodb connected :)")
+);
 
-mongoose.connection.on("error", () => console.log("nay db connexion error sorry :("));
-
+mongoose.connection.off("error", () =>
+  console.log("nay db connexion error sorry :(")
+);
