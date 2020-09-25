48f20bf27e79ddba46885a4e4299cabddb0b4466
Bouziane <bey.bouziane@live.fr>
2020-09-25 11:46:47 +0200

    Log in and out ok

--- routes/auth.js
+++ routes/auth.js
@@ -2,34 +2,30 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const exposeFlashMessage = require("../middlewares/exposeFlashMessage");
+
 const salt = 10;
 
-router.get("/signin", async (req, res, next) => {
-  res.render("signin.hbs");
-});
-
-router.post("auth/signin", async (req, res, next) => {
-  console.log('sign in', req.body);
-  const {
-    email,
-    password
-  } = req.body;
+router.post("/signin", async (req, res, next) => {
+  console.log("sign in", req.body);
+  const { email, password } = req.body;
   const foundUser = await User.findOne({
-    email: email
+    email: email,
   });
   console.log(foundUser);
 
   if (!foundUser) {
-    req.flash("error", "Invalid credentials");
-    res.redirect("signin.hbs");
+    console.log("EMPTYYY");
+    req.flash("Error", "Invalid credentials");
+    // res.redirect("signin");
   } else {
     const isSamePassword = bcrypt.compareSync(password, foundUser.password);
     if (!isSamePassword) {
       req.flash("error", "Invalid Credentials");
-      res.redirect("signin.hbs");
+      res.redirect("signin");
     } else {
       const userDocument = {
-        ...foundUser
+        ...foundUser,
       };
       console.log(userDocument);
       const userObject = foundUser.toObject();
@@ -42,39 +38,37 @@ router.post("auth/signin", async (req, res, next) => {
 });
 
 router.post("/signup", async (req, res, next) => {
-  console.log('sign UP', req.body);
+  console.log("je suis la");
+  console.log("sign UP", req.body);
   try {
     const newUser = req.body;
-    console.log('newUser: ', newUser);
+    console.log("newUser: ", newUser);
     const foundUser = await User.findOne({
-      email: newUser.email
+      email: newUser.email,
     });
-    console.log('foundUser: ', foundUser);
+    console.log("foundUser: ", foundUser);
     if (foundUser) {
-      console.log('user touver');
-      
-      res.render("/auth/signup", {
-        msg: "Email already taken"
-      });
+      console.log("user touver");
 
+      res.render("/signup", {
+        msg: "Email already taken",
+      });
     } else {
       const hashedPassword = bcrypt.hashSync(newUser.password, salt);
       newUser.password = hashedPassword;
       const user = await User.create(newUser);
-      res.redirect("auth/signin");
+      res.redirect("/signin");
     }
   } catch (error) {
     next(error);
   }
 });
 
-// router.get("/logout", async (req, res, next) => {
-//   console.log(req.session.currentUser);
-//   req.session.destroy(function (err) {
-//     // cannot access session here
-//     // console.log(req.session.currentUser);
-//     res.redirect("signin.hbs");
-//   });
-// });
+router.get("/logout", async (req, res, next) => {
+  console.log(req.session.currentUser);
+  req.session.destroy(function (err) {
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
